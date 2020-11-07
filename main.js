2ffe0f584d3c9c92f871fd7ecfac05cb2c521e61
YumeHinata <yume3099729829@gmail.com>
2020-11-07 13:54:11 +0800

    12

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("button1").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
+
+document.getElementById("button2").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
+
+document.getElementById("button3").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
+
+document.getElementById("button4").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
